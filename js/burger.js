1d3caab15eea3d8c40ca588e02138940bb84fe41
Majoxs <majoxs@gmail.com>
2021-11-26 15:11:40 +0500

    added tabs

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,7 @@
 const burgerMenu = (selectorBurger, selectorMenu, burgerActive, menuActive) => {
   const burger = document.querySelector(selectorBurger);
-  console.log('burger: ', burger);
-
   const menu = document.querySelector(selectorMenu);
-  console.log('menu: ', menu);
+
 
   burger.addEventListener('click', () => {
     burger.classList.toggle(burgerActive);
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 import burgerMenu from "./burger.js";
+import tabs from "./tabs.js";
+import renderTabs from "./renderTabs.js";
 
 burgerMenu(
   '.burger',
@@ -6,3 +8,13 @@ burgerMenu(
   'burger_active',
   'navigation__list_active'
 );
+
+tabs({
+  selectorTabsButtons: '.tabs__btn',
+  activeClassButton: 'tabs__btn_active',
+  selectorTabsElement: '.tabs__item',
+  activeClassTab: 'tabs__item_active',
+  callback: renderTabs,
+});
+
+renderTabs();
